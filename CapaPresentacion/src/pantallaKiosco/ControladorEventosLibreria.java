721ae8ef4dcde7fce1ca9673b77564a0eab419dd
Daniel Ramirez <dankramirez@outlook.com>
2020-05-25 18:04:51 -0500

    puse algunos componentes del controlador FXML

--- CapaPresentacion/src/pantallaKiosco/ControladorEventosLibreria.java
+++ CapaPresentacion/src/pantallaKiosco/ControladorEventosLibreria.java
@@ -1,23 +1,86 @@
 
 package pantallaKiosco;
 
+import entities.Linea;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ControladorEventosLibreria implements Initializable {
     
-    @FXML
-    private Label label;
+    //ETIQUETAS
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Label tituloLabel;
+    @FXML
+    private Label fechaLabel;
+    @FXML
+    private Label numeroPrestamoLabel;
+    @FXML
+    private Label seleccionarLibroLabel;
+    @FXML
+    private Label cantidadLabel;
+    @FXML
+    private Label lineasPrestamoLabel;
+    @FXML
+    private Label totalPrestamoLabel;
+    @FXML
+    private Label cantidadMonedasLabel;
+    @FXML
+    private Label denominacionLabel;
+    @FXML
+    private Label saldoDisponibleLabel;
+    @FXML
+    private Label vueltasLabel;
+    
+    //BOTONES
+    
+    @FXML
+    private Button nuevoPrestamoButton;
+    @FXML
+    private Button agregarLineaButton;
+    @FXML
+    private Button agregarMonedasButton;
+    @FXML
+    private Button terminarPrestamoButton;
+    @FXML
+    private Button generarReporteButton;
+    
+    //CHOICE BOX
+    
+    @FXML
+    private ChoiceBox<String> seleccionarLibroChoiceBox;
+    @FXML
+    private ChoiceBox<String> seleccionarDenominacionChoiceBox;
+    
+    //TEXT FIELDS
+    
+    @FXML
+    private TextField cantidadLibroTextField;
+    @FXML
+    private TextField cantidadMonedasTextField;
+    
+    //PARA LA TABLA
+    
+    @FXML
+    private TableView<Linea> tablaTableView;
+    @FXML
+    private TableColumn<Linea, String> libroTableColumn;
+    @FXML
+    private TableColumn<Linea, String> cantidadTableColumn;
+    @FXML
+    private TableColumn<Linea, String> precioLibroTableColumn;
+    @FXML
+    private TableColumn<Linea, String> subTotalTableColumn;
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
